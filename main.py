3592d784073c5369c87656d585b759a380f1b5a6
tanker1202 <130538980+tanker1202@users.noreply.github.com>
2025-05-22 07:30:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 import json
 
 app = FastAPI()
 
 with open("q-vercel-python.json", "r") as f:
     marks_db = json.load(f)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 @app.get("/api")
 async def get_marks(request: Request):
